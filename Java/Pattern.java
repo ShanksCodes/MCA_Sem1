0f51dd8f06c62c49448374acb770028633f318d3
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-11-14 20:34:22 +0530

    made the code better

--- Java/Pattern.java
+++ Java/Pattern.java
@@ -8,15 +8,22 @@ public class Pattern
 	   Scanner sc = new Scanner(System.in);
 	   
 	   int n= 5;
+	   char exitchoice='y';
+	 do{  
 	   int i=0,j=0,k=0,q=1;
-	   
+	   System.out.println("\n\n*******************************************************************\n\n");
 	   System.out.println("1.Press 1 for pattern 1");
 	   System.out.println("1.Press 2 for pattern 2");
 	   System.out.println("1.Press 3 for pattern 3");
+	   System.out.println("1.Press 4 for pattern 4");
 	   
 	   int choice= sc.nextInt();
-	   System.out.println("\n\n");
+	   
+	   System.out.print("\n\nEnter the number of rows for the pattern: ");
+	   n= sc.nextInt();
+	   
 					 
+	   
 	   switch(choice)
 	    {
 	      case 1: 
@@ -78,10 +85,39 @@ public class Pattern
 						System.out.println();
 				   }
 		          break;
+				  
+				  		  
+		  case 4: 
+		          
+				  for(i=1;i<=n;++i)
+				   {
+					   for(j=1;j<=(2*n-1);++j)
+					    {
+							if((i+j)==(n+1))
+							 {
+								 for(k=1;k<=(2*i-1);++k)
+								  {
+									  System.out.print("* ");
+									  
+									   ++j;
+								  }
+								 
+							 }
+							else System.out.print("  "); 
+						}
+						
+						System.out.println();
+				   }
+		          break;
 		
 		  default: System.out.println("\nInvalid entry!");
 	    }
+		
+	System.out.println("\nDo you want to continue?(y/n)?: ");
+	exitchoice= sc.next().charAt(0);
+	
 	   
 	   
-   }
+   }while(exitchoice=='y' || exitchoice=='Y');
+ }
  }
