92e03f5dda9731f1c598a9d56b5fb1f541e7e96f
Abhishek Tyagi <abhishektyagiofficial99@gmail.com>
2022-11-16 10:57:45 +0530

    init

--- Java/arrsum.java
+++ Java/arrsum.java
@@ -1,21 +1,22 @@
 
 
-class Sumarraytarget
+class Arrsum
  {
     public static void main(String args[])
       {
 
           int arr[] = {2,7,11,15};
+          int target=9;
           int flag=0,ind1=0,ind2=0; 
  
-          int target=9;
+          
 
-          int arr[] = {2,7,11,15};
+        // System.out.println(arr.length);
 
          for(int i=0; i<arr.length-1; ++i)
-           {
-             for(int j=(i+1); i<arr.length;++j)
-                {
+           { System.out.println("ho\n");
+             for(int j=i+1; i<arr.length;++j)
+                {       System.out.println("yo\n");
                    if(arr[i]+arr[j]==target)
                      {
                        ind1 = i; ind2=j;
@@ -24,11 +25,11 @@ class Sumarraytarget
                      }
                 }
 
-               if(flag)
+               if(flag==1)
                 break;
            }
 
-       if(flag)
+       if(flag==1)
     
            System.out.println("Target possible\nIndices are: " + ind1 + " and "+ ind2);
                
@@ -39,20 +40,10 @@ class Sumarraytarget
  
 
 
-}
+  }
 
 }
 
 
 
 
-
-
-
-
-              
-           }
-          
-      }
-
- }
