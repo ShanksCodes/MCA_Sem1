f2863d6c4f82513e2092af85cf513acfff5bcdd3
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-12-07 20:20:37 +0530

    Testing some java things

---
+++ Java/Testing.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class Testing
+  {
+		
+	public static void main(String args[])
+	 {
+				
+	    Scanner sc = new Scanner(System.in);
+		 
+		int nums[]= {1,2,2};
+		 String str="";
+		 
+		 
+        for(int i=0;i<nums.length;++i)
+          {
+              str+= nums[i];
+
+          }
+		  int intval= Integer.parseInt(str);
+		  intval++;
+		  System.out.println(intval);
+		  
+		  str= Integer.toString(intval);
+		  
+		  		  System.out.println(intval);
+
+		  
+		/*  int num2[]= new int[3];
+		  System.out.println(str);
+		
+		
+          for(int i=0;i<nums.length;++i)
+		  {
+			  num2[i]= str.charAt(i) - '0';
+		  }
+        
+         for(int i=0;i<num2.length;++i)
+			 System.out.println(num2[i]);*/
+		 
+         	  
+			 
+		 	
+			 
+	 }
+  }
